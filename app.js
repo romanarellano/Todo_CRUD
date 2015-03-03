5ebf32b73363593db391d60eb96f5f03740dcbba
romanarellano <romanarellano@yahoo.com>
2015-03-03 10:42:57 -1000

    first version

---
+++ app.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(express.static('public'));
+app.set('view engine','jade');
+app.use(methodOverride('_method'));
+
+mongoose.connect('mongodb://localhost/Todo_CRUD',function (err){
+
+  if(err)console.log(err);
+
+});
+
+var Schema = mongoose.Schema;
+
+
+var ToDoSchema = new Schema({
+
+  title: String,
+  description: String,
+  is_done: Boolean,
+  created_at: Date
+
+});
+
+var Task = mongoose.model("ToDo", ToDoSchema);
+
+
+
+
+
+
+app.get('/new_todo', function (req,res){
+
+  res.render('new_todo');
+
+  
+
+});
+
+app.get('/',function (req,res){
+
+  console.log('todo');
+  
+  Task.find(function(err,items){
+
+    if(err) throw err;
+    res.render("todos", { todos : items} );
+
+  });
+});
+
+app.post('/todos',function (req,res){
+
+  var todo = new Task ({
+     title: req.body.title,
+     description: req.body.description,
+     is_done: req.body.is_done,
+     created_at: new Date()
+  });
+
+  todo.save(function (err){
+
+    if(err) throw err;
+    console.log("ok!");
+
+     res.redirect('/');
+
+  });
+
+  
+
+});
+
+app.get('/edit/:id', function (req,res){
+
+  Task.findOne({_id : req.params.id}, function(err,todo){
+
+    if(err) throw err;
+    res.render('edit', { ToDo : todo});
+
+  });
+
+});
+
+app.put('/edit/:id', function (req,res){
+
+  Task.findOneAndUpdate({ _id : req.params.id},{
+    $set: { 
+
+      title: req.body.title,
+      description: req.body.description
+    
+    }}, function (err,todo){
+      if (err) throw err;
+      res.redirect('/');
+
+    });
+    
+  });
+
+
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
