ba1011207bbcf124e1841b64c866e2ef69b266fb
romanarellano <romanarellano@yahoo.com>
2015-03-04 14:42:24 -1000

    strike through on word made while checking

--- public/ajax.js
+++ public/ajax.js
@@ -4,62 +4,57 @@ $(document).ready(function(){
   var num=0;
 
   
-
-   
-
-  $("input[type=checkbox]").change(function(){
-    var doc_id = $(this).data('list-id');
-    //alert(doc_id);
+  $("input[type=checkbox]").change(function(what){
+     
+     var doc_id = $(this).data('list-id');
     
      var checked = $(":checked").length;
 
      var total = $("input[type=checkbox]").length;
 
-    
+     $(this).closest("li").css({"text-decoration": "line-through"});
+     
 
-    if( $(this).prop( "checked" ) ){
-      // checkbox was checked
+     if( $(this).prop( "checked" ) ){
+        // checkbox was checked
+        
+        
       
-       
         document.getElementById("countercheck").innerHTML = checked + " Complete";
 
         document.getElementById("counteruncheck").innerHTML = total - checked + " Incomplete"; 
 
-       
+   
         $.ajax('/check/'+ doc_id +'/complete', {
-          method : "PUT"
+            method : "PUT"
 
         });
 
 
-      
+    }   else{
 
-      // alert('sent PUT request to '+'/list/:id/complete');
-    }else{
+          $(this).closest("li").css({"text-decoration": "none"});
+          
+          document.getElementById("countercheck").innerHTML = checked + " Complete";
 
+          document.getElementById("counteruncheck").innerHTML = total - checked + " Incomplete";
         
-        
-        
-        document.getElementById("countercheck").innerHTML = checked + " Complete";
-
-        document.getElementById("counteruncheck").innerHTML = total - checked + " Incomplete";
-      
-      $.ajax('/check/'+doc_id+'/incomplete', {
-        method : "PUT"
-      });
-      // alert('sent PUT request to '+'/list/'+doc_id+'/uncomplete');
+          $.ajax('/check/'+doc_id+'/incomplete', {
+            method : "PUT"
+          });
+    
     }
   }); // change method
   
   // select all checkboxes with data of checked
   $("input[data-checked=true]").prop('checked', true);
 
-  var checked = $(":checked").length;
+    var checked = $(":checked").length;
 
-  var total = $("input[type=checkbox]").length;
+    var total = $("input[type=checkbox]").length;
 
-  document.getElementById("countercheck").innerHTML = $(":checked").length + " Complete";
+    document.getElementById("countercheck").innerHTML = $(":checked").length + " Complete";
 
-  document.getElementById("counteruncheck").innerHTML = total - checked + " Incomplete";
+    document.getElementById("counteruncheck").innerHTML = total - checked + " Incomplete";
 
 });
