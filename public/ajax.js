25709c08d8713247d259b6a6a76d77ff8165b434
romanarellano <romanarellano@yahoo.com>
2015-03-04 20:33:29 -1000

    added extended files

--- public/ajax.js
+++ public/ajax.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
   // select all checkboxes with data of checked
   $("input[data-checked=true]").prop('checked', true);
 
-     $("input[data-checked=true]").prop('checked', true).closest("li").css({"text-decoration":"line-through"});
+    $("input[data-checked=true]").prop('checked', true).closest("li").css({"text-decoration":"line-through"});
 
     var checked = $(":checked").length;
 
